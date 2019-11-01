95be039993868654fdaba1a2089f551a0108a67d
garybot <argot.pryer@gmail.com>
2019-10-31 18:55:44 -0700

    changed strike-through color to red

--- src/components/TodoComponents/Todo.js
+++ src/components/TodoComponents/Todo.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
 const Todo = (props) => {
-  const completed = props.item.completed ? "line-through" : "none";
+  const completed = props.item.completed ? "line-through red" : "none";
   return (
-    <li style={{textDecoration: completed}} onClick={() => props.toggleComplete(props.item.id)}>{props.item.task}</li>
+    <li className="todo-item" style={{textDecoration: completed}} onClick={() => props.toggleComplete(props.item.id)}>{props.item.task}</li>
   )
 }
 
